9fdaec96b1afa02ea2d08d73489d3ccfbc199e6a
Naruyoko <naru519372@gmail.com>
2018-07-25 20:34:24 +0900

    qwgnjfghhedfgbngf

--- javascripts/game.js
+++ javascripts/game.js
@@ -4933,7 +4933,7 @@ function gameLoop(diff) {
 
 
     if (player.infMultBuyer) {
-        var dif = (player.infinityPoints.e - player.infMultCost.e) / Math.log10(4) +1
+        var dif = Math.floor(player.infinityPoints.ln()/Math.log(4) - player.infMultCost.ln()/Math.log(4)) +1
         if (dif > 0) {
             player.infMult = player.infMult.times(Decimal.pow(2, dif))
             player.infMultCost = player.infMultCost.times(Decimal.pow(4, dif))
